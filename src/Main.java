63c9f16bde3d9f4cb97bc3953893bf7a1d3f0cfc
Vinicews <carvalho.vinicius2@gmail.com>
2024-03-27 20:50:04 -0300

    Conteudo do curso Alura

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Marverick");
+
+        String
+        int anoDeLancamento =2022;
+        boolean incluidoNoPlano = false;
+        double notaDOFilme = 9.8;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+
+    }
+}
