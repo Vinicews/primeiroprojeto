b190e1dde16dc05aa2de82d7809218d1e5da7931
Vinicius Martins <carvalho.vinicius2@gmail.com>
2024-03-31 09:35:45 -0300

    Estruturas de controle

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Esse é o Screen Match");
-        System.out.println("Filme: Top Gun: Marverick");
 
-        String
+
+        boolean incluidoNoPlano = true;
         int anoDeLancamento =2022;
-        boolean incluidoNoPlano = false;
-        double notaDOFilme = 9.8;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        double notaDoFilme = 9.8;
+        //Media calculada pelas 3 notas
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        String sinopse= "Filme de aventura com galã dos anos 80.";
 
+
+        System.out.println(media);
+
+        System.out.println(sinopse);
+
+
+        int classificacao;
+        classificacao  = (int) (media /2);
 
     }
 }
